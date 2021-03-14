25c7722399cac469d970ebaa970a52ba3fbd7839
itfelipe <itfeliperocha@gmail.com>
2021-03-14 03:21:59 -0300

    Work Done

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import Alert from 'react'
 import '../styles/tasklist.scss'
 
 import { FiTrash, FiCheckSquare } from 'react-icons/fi'
@@ -16,30 +16,58 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    
+    if(newTaskTitle){
+      const data = {
+        id: Math.random(),
+        title: newTaskTitle,
+        isComplete:false
+    }
+     setTasks(value =>[...value, data])
+         
+      
+    
+    }
+    else{
+      alert("Cannot add a task without a title")
+    }
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const markedTasks = tasks.map(tasks => tasks.id === id ?{
+        ...tasks,
+        isComplete: !tasks.isComplete
+    } : tasks)    
+    setTasks(markedTasks)
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const nonRemovedTasks = tasks.filter(tasks => tasks.id !== id)
+    setTasks(nonRemovedTasks)
+  }
+  function removeMarkedTasks(id: number) {
+    const markedRemovedTasks = tasks.filter(tasks => tasks.isComplete === false)
+    setTasks(markedRemovedTasks)
   }
 
   return (
     <section className="task-list container">
       <header>
-        <h2>Minhas tasks</h2>
+        <h2>My tasks</h2>
 
         <div className="input-group">
           <input 
             type="text" 
-            placeholder="Adicionar novo todo" 
+            placeholder="Add new todo" 
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
           <button type="submit" data-testid="add-task-button" onClick={handleCreateNewTask}>
             <FiCheckSquare size={16} color="#fff"/>
+          </button>
+          <button type="submit" data-testid="remove-marked-tasks" onClick={removeMarkedTasks}>
+            <FiCheckSquare size={16} color="red"/>
           </button>
         </div>
       </header>
@@ -64,6 +92,7 @@ export function TaskList() {
               <button type="button" data-testid="remove-task-button" onClick={() => handleRemoveTask(task.id)}>
                 <FiTrash size={16}/>
               </button>
+              
             </li>
           ))}
           
